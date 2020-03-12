0719b4508052279ffe704ad8c2168c9dae9a7c4c
[Miguel Blanco] <[miguelrobertoblanco@gmail.com]>
2020-03-12 05:28:36 -0600

    Pasion al Codigo

--- src/proyecto/eddii/ProyectoEDDII.java
+++ src/proyecto/eddii/ProyectoEDDII.java
@@ -5,6 +5,8 @@
  */
 package proyecto.eddii;
 
+import java.util.Scanner;
+
 /**
  *
  * @author migue
@@ -15,7 +17,19 @@ public class ProyectoEDDII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Calculadora Simple");
+        System.out.println("Ingrese su primer numero: ");
+        int x = leer.nextInt();
+        System.out.println("Ingrese su segundo numero: ");
+        int p = leer.nextInt();
+        String j = "";
+        j += x;
+        j += p;
+        System.out.println("La respuesta es " + j);
+        System.out.println("PROGRAMAR ES MI PASION");
+
         // TODO code application logic here
     }
-    
+
 }
