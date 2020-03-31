3aca5df825703a6da09990e39878a7f903de2824
FranciscoPagoaga <francisco.pagoaga14@gmail.com>
2020-03-31 12:44:56 -0600

    asd

--- src/proyecto/eddii/Main.java
+++ src/proyecto/eddii/Main.java
@@ -1486,6 +1486,7 @@ public class Main extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Main().setVisible(true);
+                
             }
         });
     }
